8f6f936e2cc83d659b243e156046191c1792c56a
Sean Hull <hullsean@gmail.com>
2017-05-18 17:15:52 -0400

    added efs support, to mount external homedirs

--- iheavy-user-data.sh
+++ iheavy-user-data.sh
@@ -22,8 +22,6 @@ export COMPOSER_HOME=/root
 
 cd /root
 
-# this fixes the composer install.
-# we also put it in /usr/bin where it's found in the path
 echo "installing composer..."
 wget https://getcomposer.org/installer
 mv installer composer-setup.php
@@ -39,10 +37,15 @@ fi
 
 # fetch config files from private S3 folder
 # need to fetch from git
-aws s3 cp s3://iheavy-config/iheavy_files.zip .
+#aws s3 cp s3://iheavy-config/iheavy_files.zip .
+export HOME=/root
+git config --global credential.UseHttpPath true
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git clone https://git-codecommit.us-east-1.amazonaws.com/v1/repos/iheavy
+cd iheavy
 
 # unzip files
-unzip iheavy_files.zip 
+#unzip iheavy_files.zip 
 
 # use composer to get wordpress & plugins
 /usr/bin/composer update
@@ -72,4 +75,7 @@ service httpd restart
 
 # allow apache to create uploads & any files inside wp-content
 chown -R apache /var/www/html/wp-content
+
+# mount EFS for external /homedirs!
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 172.30.4.158:/ efs
 
