d9431660be6bfa2595c1d696ae66892da318c350
Sean Hull <hullsean@gmail.com>
2018-04-28 16:54:40 -0400

    iheavy-iac reboot script - also can stop/start server

---
+++ reboot_box.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+export instance_id = i-05e9dce2e0a1530e2
+
+# get instance id
+# aws ec2 describe-instances | grep tag name = iheavy-iac
+# set instance_id variable
+
+#
+# hack for now...
+#
+aws ec2 reboot-instances --instance-ids $instance_id
+
+
+# this script should:
+# find the instance-id by tag name=iheavy-iac
+# then run above command by variable
+
+# need cli switch to do stop/start cycle
+# that option needs to loop until the instance is stopped
+# so it's a bit trickier
+
+# to stop:
+# aws ec2 stop-instances --instance-ids $instance_id
+
+# check status
+# aws ec2 describe-instances | grep "State = stopped"
+
+#
+# to start instance
+# aws ec2 start-instances --instance-ids $instance_id
+
+
