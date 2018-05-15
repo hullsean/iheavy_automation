87f720dfb3cb9c01c434dfe3531c515b2c32febc
Sean Hull <hullsean@gmail.com>
2018-05-14 23:59:38 -0400

    reboot_box now fetches instanceid by tag=iheavy-iac and also does a stop/start cycle instead of reboot which somestimes doesnt work

--- reboot_box.sh
+++ reboot_box.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 
-export instance_id=i-05e9dce2e0a1530e2
+#export instance_id=i-05e9dce2e0a1530e2
+
+# look up the instance_id based on tag of "iheavy-iac"
+export instance_id=`aws ec2 describe-instances --filter Name=tag:Name,Values=iheavy-iac | grep InstanceId | cut -d ':' -f2 | sed 's/,//' | sed 's/\"//g'`
 
 # get instance id
 # aws ec2 describe-instances | grep tag name = iheavy-iac
@@ -10,7 +13,7 @@ export instance_id=i-05e9dce2e0a1530e2
 #
 # hack for now...
 #
-aws ec2 reboot-instances --instance-ids $instance_id
+#aws ec2 reboot-instances --instance-ids $instance_id
 
 
 # this script should:
@@ -22,13 +25,14 @@ aws ec2 reboot-instances --instance-ids $instance_id
 # so it's a bit trickier
 
 # to stop:
-# aws ec2 stop-instances --instance-ids $instance_id
+aws ec2 stop-instances --instance-ids $instance_id
 
 # check status
 # aws ec2 describe-instances | grep "State = stopped"
+sleep 60
 
 #
 # to start instance
-# aws ec2 start-instances --instance-ids $instance_id
+aws ec2 start-instances --instance-ids $instance_id
 
 
